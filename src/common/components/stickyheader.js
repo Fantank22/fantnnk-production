b54788f04f8e81902af0af7c3adc640657378e15
Md Abdullah Al Noman <yabdullahalnoman@gmail.com>
2022-05-31 16:36:52 +0600

    Token added to home page routes

--- src/common/components/stickyheader.js
+++ src/common/components/stickyheader.js
@@ -1,8 +1,7 @@
 import React, { useContext } from "react";
-import { View, Image, TouchableOpacity, StatusBar } from "react-native";
+import { View, Image, TouchableOpacity, Platform, Text } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Constants from "expo-constants";
-import { Platform } from "react-native";
 
 import UserContext from "../../context/auth";
 
@@ -24,26 +23,75 @@ export const StickyHeader = ({ navigation }) => {
         marginTop: Platform.OS === "ios" ? -50 : 0,
       }}
     >
-      <TouchableOpacity onPress={() => navigation.openDrawer()}>
-        <Ionicons
-          name="list-outline"
-          size={20}
-          style={{ width: 20, height: 20 }}
-          color="#fff"
-        />
-      </TouchableOpacity>
-      <Image
-        source={require("../assets/images/logo.png")}
-        style={{ height: 40, width: 40, resizeMode: "cover" }}
-      />
-      <View style={{ display: "flex", flexDirection: "row" }}>
-        {user !== null && (
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
           <Ionicons
-            name="person-outline"
+            name="list-outline"
             size={20}
-            style={{ width: 20, height: 20, marginRight: 10 }}
+            style={{ width: 20, height: 20 }}
             color="#fff"
           />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={{ height: 40, width: 40, resizeMode: "cover" }}
+        />
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        {user !== null && (
+          <>
+            <View
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "row",
+                backgroundColor: "#1A1A1A",
+                borderColor: "#404040",
+                borderWidth: 2,
+                marginRight: 10,
+                paddingHorizontal: 10,
+                paddingVertical: 4,
+                borderRadius: 16,
+              }}
+            >
+              <Image
+                source={require("../assets/images/artistlist/fitbit-token.png")}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: "cover",
+                  marginRight: 5,
+                }}
+              />
+              <Text style={{ color: "#fff", fontSize: 14 }}>100</Text>
+            </View>
+            <Ionicons
+              name="person-outline"
+              size={20}
+              style={{ width: 20, height: 20, marginRight: 5 }}
+              color="#fff"
+            />
+          </>
         )}
 
         <Ionicons
